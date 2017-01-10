0b03429b67f5e4a1f42cc9310b2ee22130843471
Andrei Vacaroiu <vacaroiu.andrei@gmail.com>
2017-01-10 18:57:59 +0200

    first try transform into cte

--- sql/_06_manipulation/_01_select/_001_inner_join/cases/1049.sql
+++ sql/_06_manipulation/_01_select/_001_inner_join/cases/1049.sql
@@ -1,44 +1,36 @@
---+ holdcas on;
--- [er]tests complex select with special collections 
-
 call login('dba', '') on class db_user;
 create user user1;
 create user user2;
-
 call login('user1', '') on class db_user;
-
-   create table xoo ( id int);
-   insert into xoo values(10000);
-   grant select on xoo to user2;
-
+create table xoo ( id int);
+insert into xoo values(10000);
+WITH cte as (
+grant select on xoo to user2)
+SELECT * FROM cte;
 call login('user2', '') on class db_user;
-
-   create table zoo ( id int, x xoo);
-   insert into zoo values(2, (select xoo from xoo));
-
+create table zoo ( id int, x xoo);
+WITH cte as (
+insert into zoo values(2, (select xoo from xoo)))
+SELECT * FROM cte;
 call login('user1', '') on class db_user;
-
-   revoke select on xoo from user2;
-
+WITH cte as (
+revoke select on xoo from user2)
+SELECT * FROM cte;
 call login('user2', '') on class db_user;
-   select x into :oid from zoo;
-
-   create class poo ( id int);
-
-   select :oid.id from db_root;
-   insert into poo values(:oid.id);
-   select * from poo;
-
-
-
-
-
+WITH cte as (
+select x into :oid from zoo)
+SELECT * FROM cte;
+create class poo ( id int);
+WITH cte as (
+select :oid.id from db_root)
+SELECT * FROM cte;
+insert into poo values(:oid.id);
+WITH cte as (
+select * from poo)
+SELECT * FROM cte;
 call login('dba', '') on class db_user;
-
 drop xoo;
 drop poo;
 drop zoo;
 drop user user1;
 drop user user2;
-
---+ holdcas off;
