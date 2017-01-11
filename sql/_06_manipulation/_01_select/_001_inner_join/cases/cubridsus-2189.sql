cc35ec7e53fac8f2226106556f99e997f54f0106
Andrei Vacaroiu <vacaroiu.andrei@gmail.com>
2017-01-11 16:15:53 +0200

    Revert "filter queries better"
    This reverts commit b9fe958cb5d5cbcd5d9bf5959af087b5f89f25c5.

--- sql/_06_manipulation/_01_select/_001_inner_join/cases/cubridsus-2189.sql
+++ sql/_06_manipulation/_01_select/_001_inner_join/cases/cubridsus-2189.sql
@@ -1,10 +1,9 @@
+--this cases was added for cubridsus-2189
 create table foo (i int primary key, j int);
 create unique index u_idx on foo (i, j);
-create index idx on foo (i, j desc);
+create index idx on foo (i, j desc); 
 insert into foo values (1,1);
 insert into foo values (2,2);
 insert into foo values (3,3);
-WITH cte as (
-select * from foo order by 1,2)
-SELECT * FROM cte;
+select * from foo order by 1,2;
 drop table foo;
